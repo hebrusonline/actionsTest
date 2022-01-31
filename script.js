a5d0377093aa7ed28a8db4b941f3929c4655df3e
Baumgärtner <baumga56@ads.uni-passau.de>
2022-01-31 18:15:20 +0100

    wochentag feature

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-function setup(){
+function setup() {
 	
-	//initialize clock
-	clock();
+	//  initialize clock
+    clock();
 	
-	//update clock every second!
-	var inc = 1000;
-	setInterval(clock, inc);
+	//  update clock every second!
+    var inc = 1000;
+    setInterval(clock, inc);
 	
-	//initialize weekday
-	wochentag();
+	//  initialize weekday
+    wochentag();
 }
 
 /*src: https://codepen.io/dope/pen/KJYMZz*/
 function clock() {
-  const date = new Date();
+    const date = new Date();
 
-  const hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
+    const hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
   
-  const hour = hours * 30;
-  const minute = minutes * 6;
-  const second = seconds * 6;
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
   
-  document.querySelector(".hour").style.transform = `rotate(${hour}deg)`;
-  document.querySelector(".minute").style.transform = `rotate(${minute}deg)`;
-  document.querySelector('.second').style.transform = `rotate(${second}deg)`;
+    document.querySelector(".hour").style.transform = `rotate(${hour}deg)`;
+    document.querySelector(".minute").style.transform = `rotate(${minute}deg)`;
+    document.querySelector('.second').style.transform = `rotate(${second}deg)`;
   
-  if(seconds<10){
-	  seconds = '0' + seconds;
-  }
+    if(seconds<10){
+        seconds = '0' + seconds;
+    }
   
     if(minutes<10){
-	  minutes = '0' + minutes;
-  }
+        minutes = '0' + minutes;
+    }
   
-  document.querySelector('.clock_label').innerHTML = 'Es ist '+hours+':'+minutes+':'+seconds+' Uhr.';
+    document.querySelector('.clock_label').innerHTML = 'Es ist '+ hours + ':' + minutes+':'+ seconds + ' Uhr.';
 }
 
-
 function wochentag() {
-	const date = new Date();
-	const wochentag = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
-	let day = date.getDay();
+    const date = new Date();
+    const wochentag = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+    let day = date.getDay();
 	
-	document.querySelector('.week_label').innerHTML = 'An einem ' + wochentag[day]+'.';
+    document.querySelector('.week_label').innerHTML = 'An einem ' + wochentag[day]+ '.';
 }
