0a35de149af09bdd5d8f68483737e345c488cdd8
Baumgärtner <baumga56@ads.uni-passau.de>
2022-01-31 19:01:21 +0100

    first commit

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function clock () {
 
   document.querySelector('.clock_label').innerHTML = 'Es ist ' + hours + ':' + minutes + ':' + seconds + ' Uhr.';
 }
-
+/*
 function wochentag () {
   const date = new Date();
   const wochentag = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
@@ -44,3 +44,4 @@ function wochentag () {
 
   document.querySelector('.week_label').innerHTML = 'An einem ' + wochentag[day] + '.';
 }
+*/
