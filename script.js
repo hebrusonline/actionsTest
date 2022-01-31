2ce96743acfb41432df448c78e1baab1f54b8b0d
Baumgärtner <baumga56@ads.uni-passau.de>
2022-01-31 18:48:42 +0100

    wochentag feature

--- script.js
+++ script.js
@@ -21,26 +21,26 @@ function clock () {
   const hour = hours * 30;
   const minute = minutes * 6;
   const second = seconds * 6;
-  
+
   document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
   document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
   document.querySelector('.second').style.transform = `rotate(${second}deg)`;
-  
-  if (seconds < 10){
+
+  if (seconds < 10) {
     seconds = '0' + seconds;
   }
 
-  if (minutes < 10){
+  if (minutes < 10) {
     minutes = '0' + minutes;
   }
-  
-  document.querySelector('.clock_label').innerHTML = 'Es ist ' + hours + ':' + minutes+':'+ seconds + ' Uhr.';
+
+  document.querySelector('.clock_label').innerHTML = 'Es ist ' + hours + ':' + minutes + ':' + seconds + ' Uhr.';
 }
 
 function wochentag () {
   const date = new Date();
-  const wochentag = ["Sonntag", 'Montag', 'Dienstag', "Mittwoch", 'Donnerstag', 'Freitag', 'Samstag'];
-  const day = date.getDay()
+  const wochentag = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+  const day = date.getDay();
 
   document.querySelector('.week_label').innerHTML = 'An einem ' + wochentag[day] + '.';
 }
