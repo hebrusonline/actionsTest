a057774420168d29c137fc0aa62ca408b993e5e3
Baumgärtner <baumga56@ads.uni-passau.de>
2022-02-01 15:28:22 +0100

    wochentag feature

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function clock () {
 
   document.querySelector('.clock-label').innerHTML = 'Es ist ' + hours + ':' + minutes + ':' + seconds + ' Uhr.';
 }
-/*
+
 function wochentag () {
   const date = new Date();
   const wochentag = ['Sonntag', "Montag", 'Dienstag', 'Mittwoch', "Donnerstag", 'Freitag', 'Samstag'];
@@ -47,4 +47,3 @@ function wochentag () {
 
   document.querySelector('.week-label').innerHTML = 'An einem ' + wochentag[day] + '.';
 }
-*/
